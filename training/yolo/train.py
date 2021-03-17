371e3a28a33b3af336c62cf0fa09227f29166fa1
Aneesh Bal <aneesh.s.bal@gmail.com>
2021-03-17 15:15:05 -0700

    update

--- training/yolo/train.py
+++ training/yolo/train.py
@@ -22,8 +22,8 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 import test  # import test.py to get mAP after each epoch
-from models.experimental import attempt_load
-from models.yolo import Model
+from .models.experimental import attempt_load
+from .models.yolo import Model
 from utils.autoanchor import check_anchors
 from utils.datasets import create_dataloader
 from utils.general import labels_to_class_weights, increment_path, labels_to_image_weights, init_seeds, \
