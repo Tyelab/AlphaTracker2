2626d5dc6d46d02f7c35f9f03117e37b0bc3edd9
Aneesh Bal <aneesh.s.bal@gmail.com>
2021-03-17 15:27:00 -0700

    Update train.py

--- training/yolo/train.py
+++ training/yolo/train.py
@@ -474,7 +474,7 @@ if __name__ == '__main__':
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--quad', action='store_true', help='quad dataloader')
     parser.add_argument('--linear-lr', action='store_true', help='linear LR')
-    opt = parser.parse_args()
+    opt = parser.parse_args(args=[])
 
     # Set DDP variables
     opt.world_size = int(os.environ['WORLD_SIZE']) if 'WORLD_SIZE' in os.environ else 1
