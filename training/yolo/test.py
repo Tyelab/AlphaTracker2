eee8c82f463929ee423d535dc2545366a45f4732
Aneesh Bal <aneesh.s.bal@gmail.com>
2021-03-17 18:56:06 -0700

    Update test.py

--- training/yolo/test.py
+++ training/yolo/test.py
@@ -9,13 +9,13 @@ import torch
 import yaml
 from tqdm import tqdm
 
-from models.experimental import attempt_load
-from utils.datasets import create_dataloader
-from utils.general import coco80_to_coco91_class, check_dataset, check_file, check_img_size, check_requirements, \
+from .models.experimental import attempt_load
+from .utils.datasets import create_dataloader
+from .utils.general import coco80_to_coco91_class, check_dataset, check_file, check_img_size, check_requirements, \
     box_iou, non_max_suppression, scale_coords, xyxy2xywh, xywh2xyxy, set_logging, increment_path, colorstr
-from utils.metrics import ap_per_class, ConfusionMatrix
-from utils.plots import plot_images, output_to_target, plot_study_txt
-from utils.torch_utils import select_device, time_synchronized
+from .utils.metrics import ap_per_class, ConfusionMatrix
+from .utils.plots import plot_images, output_to_target, plot_study_txt
+from .utils.torch_utils import select_device, time_synchronized
 
 
 def test(data,
